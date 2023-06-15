ec864e3cbc1732831faca8d9a6438c0a23923de3
koushik.pathuri <119720431+pathurikoushik@users.noreply.github.com>
2023-06-15 10:29:13 +0530

    import java.util.*; import java.io.*;
    public class Codemind
    {
        public static void main(String args[])
        {
            double p,t,r;
            Scanner s=new Scanner(System.in);
            p=s.nextDouble();
            r=s.nextDouble();
            t=s.nextDouble(); 
            double c = p *(Math.pow((1 + (r / 100)), t));
            System.out.format("%.2f",c);
        }
    }

---
+++ Compound_interest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+ 
+public class Codemind
+{
+    public static void main(String args[])
+    {
+        double p,t,r;
+        Scanner s=new Scanner(System.in);
+        p=s.nextDouble();
+        r=s.nextDouble();
+        t=s.nextDouble(); 
+        double c = p *(Math.pow((1 + (r / 100)), t));
+        System.out.format("%.2f",c);
+    }
+}
